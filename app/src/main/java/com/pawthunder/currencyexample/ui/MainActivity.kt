8d5f1af9e5cc187058278c8f8181c88ae6dedde4
pawthunder <petergrajko@gmail.com>
2020-05-24 17:24:14 +0200

    Implement UI layouts

--- app/src/main/java/com/pawthunder/currencyexample/ui/MainActivity.kt
+++ app/src/main/java/com/pawthunder/currencyexample/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.pawthunder.currencyexample.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 import com.pawthunder.currencyexample.R
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), HasAndroidInjector {
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar as MaterialToolbar)
     }
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
