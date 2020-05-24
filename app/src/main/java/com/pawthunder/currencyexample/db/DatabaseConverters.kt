77408b9e02403e7ccb47f5ed000f7e6cfe96358b
pawthunder <petergrajko@gmail.com>
2020-05-24 20:35:42 +0200

    Add currency flags images

---
+++ app/src/main/java/com/pawthunder/currencyexample/db/DatabaseConverters.kt
@@ -0,0 +1,15 @@
+package com.pawthunder.currencyexample.db
+
+import androidx.room.TypeConverter
+import com.pawthunder.currencyexample.ui.CurrencyShortName
+
+object DatabaseConverters {
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToCurrencyShortName(name: String) = CurrencyShortName.find(name)
+
+    @TypeConverter
+    @JvmStatic
+    fun currencyShortNameToString(currency: CurrencyShortName) = currency.key
+}
---
+++ app/src/main/java/com/pawthunder/currencyexample/ui/CurrencyShortName.kt
@@ -0,0 +1,42 @@
+package com.pawthunder.currencyexample.ui
+
+enum class CurrencyShortName(val key: String) {
+    AUD("AUD"),
+    BGN("BGN"),
+    BRL("BRL"),
+    CAD("CAD"),
+    CHF("CHF"),
+    CNY("CNY"),
+    CZK("CZK"),
+    DKK("DKK"),
+    EUR("EUR"),
+    GBP("GBP"),
+    HKD("HKD"),
+    HRK("HRK"),
+    HUF("HUF"),
+    IDR("IDR"),
+    ILS("ILS"),
+    INR("INR"),
+    ISK("ISK"),
+    JPY("JPY"),
+    KRW("KRW"),
+    MXN("MXN"),
+    MYR("MYR"),
+    NOK("NOK"),
+    NZD("NZD"),
+    PHP("PHP"),
+    PLN("PLN"),
+    RON("RON"),
+    RUB("RUB"),
+    SEK("SEK"),
+    SGD("SGD"),
+    THB("THB"),
+    USD("USD"),
+    ZAR("ZAR");
+
+    companion object {
+        private val map = values().associateBy(CurrencyShortName::key)
+
+        fun find(key: String?) = map[key] ?: EUR
+    }
+}
